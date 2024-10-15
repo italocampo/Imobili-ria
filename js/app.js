ffbb4620386586eace44549dcb84334a7993c3df
italocampo <italosouzacampos99@gmail.com>
2024-10-15 15:52:27 -0300

    Estrutura básica

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-alert("Oi");
+// Simula um Banco de Dados
+const listaImoveis = [
+  {
+    id: 1,
+    titulo: "Apto 3 dorm. Papicu",
+    descricao:
+      "Lindo apartamento de 3 dormitórios localizado num dos melhores bairros de Fortaleza.",
+    valor: "R$ 500.000",
+    area: "90 m²",
+    quartos: "3",
+    tipo: "apartamento", // apto,casa,terreno
+    localizacao: "Av. Santos Dummont, Papicu",
+    venda_aluguel: "venda", // Aluguel
+    finalidade: "residencial", // Comercial
+    fotos: ["link1", "link2", "link3", "link4", "link5"],
+  },
+];
