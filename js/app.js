77b80177263f0e20f70fbe53846800ec6820b6f2
italocampo <italosouzacampos99@gmail.com>
2024-10-16 15:03:49 -0300

    Att

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-// Simula um  JSON online - Banco de Dados -
+// Simula um JSON online (Banco de Dados)
 const listaImoveis = [
   {
     id: 1,
-    titulo: "Apto. 3 dorm. Papicu",
+    titulo: "Apart. 3 dorm. Papicu",
     descricao:
       "Lindo apartamento de 3 dormitórios localizado num dos melhores bairros de Fortaleza.",
-    valor: 500.0,
-    area: "90",
-    quartos: "3",
-    tipo: "apartamento", // apto,casa,terreno
-    localizacao: "Av. Santos Dummont, Papicu",
-    mapa: "link do maps",
-    venda_aluguel: "venda", // Aluguel
-    finalidade: "residencial", // Comercial
+    valor: 800000,
+    area: 90,
+    quartos: 3,
+    tipo: "apartamento", // casa, terreno
+    localizacao: "Santos Dummont, Papicu",
+    mapa: "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.3752203335343!2d-38.52586952782257!3d-3.728109443164852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c74881b09b88ef%3A0xe6f7c3f410959f98!2sAv.%20Santos%20Dumont!5e0!3m2!1spt-BR!2sbr!4v1729100381171!5m2!1spt-BR!2sbr' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>",
+    venda_aluguel: "venda", // aluguel
+    finalidade: "residencial", // comercial
     fotos: [
       "https://id725a57.s3.amazonaws.com/fotos/i00035801.jpeg", // padrão [0]
       "https://images.pexels.com/photos/206172/pexels-photo-206172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
@@ -22,60 +22,65 @@ const listaImoveis = [
     ],
     status: true,
   },
+  {
+    id: 2,
+    titulo: "Apart. 2 dorm. Aldeota",
+    descricao:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias earum numquam minus atque voluptatibus libero ut doloribus voluptas quo reiciendis.",
+    valor: 500000,
+    area: 60,
+    quartos: 2,
+    tipo: "apartamento", // casa, terreno
+    localizacao: "Dom Luis, Aldeota",
+    mapa: "link do maps",
+    venda_aluguel: "venda", // aluguel
+    finalidade: "residencial", // comercial
+    fotos: [
+      "https://images.pexels.com/photos/206172/pexels-photo-206172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", // padrão [0]
 
-  {
-    id: 1,
-    titulo: "Apart. 3 dorm. Papicu",
-    descricao:
-      "Lindo apartamento de 3 dormitórios localizado num dos melhores bairros de Fortaleza.",
-    valor: 500.0,
-    area: 90,
-    quartos: 3,
-    tipo: "apartamento", // casa, terreno
-    localizacao: "Santos Dummont, Papicu",
-    mapa: "link do maps",
-    venda_aluguel: "venda", // aluguel
-    finalidade: "residencial", // comercial
-    fotos: [
-      "https://id725a57.s3.amazonaws.com/fotos/i00035801.jpeg", // padrão [0]
-      "https://images.pexels.com/photos/206172/pexels-photo-206172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://id725a57.s3.amazonaws.com/fotos/i00035801.jpeg",
+
       "https://img.freepik.com/fotos-premium/uma-casa-branca-encantadora-com-um-telhado-vermelho-brilhante-e-uma-janela-azul_1258321-339.jpg?w=360",
+
       "link4",
+
       "link5",
     ],
     status: true,
   },
   {
-    id: 1,
-    titulo: "Apart. 3 dorm. Papicu",
+    id: 3,
+    titulo: "Apart. 3 dorm. Caucaia",
     descricao:
-      "Lindo apartamento de 3 dormitórios localizado num dos melhores bairros de Fortaleza.",
-    valor: 500.0,
-    area: 90,
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam ullam sint saepe aut autem dolore ut voluptate vero voluptatibus temporibus.",
+    valor: 455000,
+    area: 70,
     quartos: 3,
     tipo: "apartamento", // casa, terreno
-    localizacao: "Santos Dummont, Papicu",
+    localizacao: "Rua Alto do Garrote, Caucaia",
     mapa: "link do maps",
     venda_aluguel: "venda", // aluguel
     finalidade: "residencial", // comercial
     fotos: [
-      "https://id725a57.s3.amazonaws.com/fotos/i00035801.jpeg", // padrão [0]
+      "https://img.freepik.com/fotos-premium/uma-casa-branca-encantadora-com-um-telhado-vermelho-brilhante-e-uma-janela-azul_1258321-339.jpg?w=360", // padrão [0]
+
+      "https://id725a57.s3.amazonaws.com/fotos/i00035801.jpeg",
+
       "https://images.pexels.com/photos/206172/pexels-photo-206172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      "https://img.freepik.com/fotos-premium/uma-casa-branca-encantadora-com-um-telhado-vermelho-brilhante-e-uma-janela-azul_1258321-339.jpg?w=360",
+
       "link4",
+
       "link5",
     ],
     status: true,
   },
 ];
 
-// Percorre a lista de Imóveis
-
+// Percorre a Lista de Imóveis
 listaImoveis.forEach((imovel) => {
   // console.log(imovel);
 
-  // Cria o card imovel
-
+  // Cria o "card" do imóvel
   const cardImovel = document.createElement("article");
   cardImovel.classList.add("imovel");
   document.querySelector("#imoveis").appendChild(cardImovel);
@@ -89,18 +94,17 @@ listaImoveis.forEach((imovel) => {
   img.setAttribute("src", imovel.fotos[0]);
   divFoto.appendChild(img);
 
-  //Conteudo
-
+  // Conteúdo
   const divConteudo = document.createElement("div");
   divConteudo.classList.add("conteudo");
-  cardImovel.appendChild(divConteudo); // Adiciona o conteudo no card
+  cardImovel.appendChild(divConteudo); // Adiciona o conteúdo no card
 
   const titulo = document.createElement("h3");
   titulo.textContent = imovel.titulo;
   divConteudo.appendChild(titulo);
 
   const descricao = document.createElement("p");
-  descricao.textContent = imovel.titulo;
+  descricao.textContent = imovel.descricao;
   divConteudo.appendChild(descricao);
 
   // Info
@@ -111,19 +115,23 @@ listaImoveis.forEach((imovel) => {
   const divDados = document.createElement("div");
   divInfo.appendChild(divDados);
 
-  const localizacao = document.createElement("span");
-  localizacao.textContent = imovel.localizacao;
+  const localizacao = document.createElement("div");
+  localizacao.textContent = `Localização: ${imovel.localizacao}`;
   divDados.appendChild(localizacao);
 
   const quartos = document.createElement("span");
-  quartos.textContent = imovel.quartos;
+  quartos.textContent = `Quartos: ${imovel.quartos} | `;
   divDados.appendChild(quartos);
 
   const area = document.createElement("span");
-  area.textContent = imovel.area;
+  area.textContent = `Área: ${imovel.area} m²`;
   divDados.appendChild(area);
 
   const divValor = document.createElement("div");
-  divValor.textContent = `R$ ${imovel.valor.toFixed(2)}`;
-  divInfo.appendChild(divValor);
+  divValor.classList.add("valor-imovel");
+  divValor.textContent = `${imovel.valor.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  })}`;
+  divDados.appendChild(divValor);
 });
